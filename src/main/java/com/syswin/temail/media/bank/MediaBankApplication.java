7b8c8cf2b15cc353fad731d88bbd30cfa096fa96
李晨锋 <lichenfeng@syswin.com>
2018-09-06 16:00:36 +0800

    修改打包方式为war

--- src/main/java/com/syswin/temail/media/bank/MediaBankApplication.java
+++ src/main/java/com/syswin/temail/media/bank/MediaBankApplication.java
@@ -2,12 +2,18 @@ package com.syswin.temail.media.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MediaBankApplication {
+public class MediaBankApplication extends SpringBootServletInitializer {
 
-  public static void main(String[] args) {
+  public static void main(String[] args)  {
     SpringApplication.run(MediaBankApplication.class, args);
   }
 
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(MediaBankApplication.class);
+  }
 }
