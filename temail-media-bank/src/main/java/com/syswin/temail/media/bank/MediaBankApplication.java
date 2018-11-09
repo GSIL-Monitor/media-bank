06e63b1190cef483341451b81bbc3545f8916b06
王选中 <wangxuanzhong@syswin.com>
2018-11-09 10:40:09 +0800

    fastemoji

--- temail-media-bank/src/main/java/com/syswin/temail/media/bank/MediaBankApplication.java
+++ temail-media-bank/src/main/java/com/syswin/temail/media/bank/MediaBankApplication.java
@@ -2,8 +2,10 @@ package com.syswin.temail.media.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.syswin.temail")
 public class MediaBankApplication {
 
   public static void main(String[] args) {
