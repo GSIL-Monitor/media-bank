8947b1e10f8520dd0583d857ba5cb2b2ea6fc619
姚华成 <yaohuacheng@syswin.com>
2018-12-14 11:23:24 +0800

    use @Configuration

--- temail-bank-core/src/main/java/com/syswin/temail/media/bank/service/ufile/UFileConfiguration.java
+++ temail-bank-core/src/main/java/com/syswin/temail/media/bank/service/ufile/UFileConfiguration.java
@@ -2,23 +2,23 @@ package com.syswin.temail.media.bank.service.ufile;
 
 import cn.ucloud.ufile.UFileSDK;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 /**
  * @author 姚华成
  * @date 2018-12-12
  */
 @Slf4j
-@Profile("ufile")
+//@Profile("ufile")
 @Configuration
 @EnableConfigurationProperties(UFileProperties.class)
 public class UFileConfiguration {
 
   @Bean
-//  @ConditionalOnProperty(value = "app.mediabank.dfs", havingValue = "ufile")
+  @ConditionalOnProperty(value = "app.mediabank.dfs", havingValue = "ufile")
   public UFileFileService fileService(UFileProperties properties) {
     log.info("------------------------------This mediabank use UFile storage!------------------------------");
     return new UFileFileService(ufileSDK(properties), properties.getTokenPrefix());
